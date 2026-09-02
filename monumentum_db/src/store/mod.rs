3793683e00a009239947b3fc3e041e11daea7569
mroczect <mroczect@proton.me>
2026-09-02 20:18:19 +0700

    refactor(store): rename serialize to serde module

--- monumentum_db/src/store/mod.rs
+++ monumentum_db/src/store/mod.rs
@@ -1,13 +1,13 @@
 pub mod append_log;
 pub mod file;
 pub mod recovery;
-pub mod serialize;
+pub mod serde;
 pub mod storage;
 pub mod wal;
 
 pub use append_log::*;
 pub use file::*;
 pub use recovery::*;
-pub use serialize::*;
+pub use serde::*;
 pub use storage::*;
 pub use wal::*;
