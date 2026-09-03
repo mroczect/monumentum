58a84bf34140777c58fe2205278ca36f83a390eb
mroczect <mroczect@proton.me>
2026-09-03 20:13:54 +0700

    feat(workbook): add query module and method

--- monumentum_workbook/src/lib.rs
+++ monumentum_workbook/src/lib.rs
@@ -6,9 +6,10 @@ use monumentum_db::store::storage::StorageEngine;
 use monumentum_query::formula::{FunctionImpl, FunctionRegistry};
 mod error;
 pub mod menu;
+pub mod query;
 pub mod transaction;
-
 pub use error::WorkbookError;
+use monumentum_db::core::row::Row;
 
 #[derive(Debug)]
 pub struct Workbook<S: StorageEngine> {
@@ -136,5 +137,9 @@ impl<S: StorageEngine> Workbook<S> {
             ));
         }
         Ok(())
+    }
+
+    pub fn query<'a>(&'a self, sheet: &str) -> query::Query<'a, S, fn(&Row) -> bool> {
+        query::Query::new(self, sheet)
     }
 }
