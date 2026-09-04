5758c00928642c4b116b2ece2179011b9770043b
mroczect <mroczect@proton.me>
2026-09-05 02:09:24 +0700

    feat(core): add page and buffer pool modules

--- monumentum_core/src/lib.rs
+++ monumentum_core/src/lib.rs
@@ -4,8 +4,11 @@ extern crate alloc;
 
 use monumentum_handler as _;
 
+pub mod buffer_pool;
 pub mod catalog;
 pub mod index;
+pub mod page;
+pub mod pager;
 pub mod serde;
 pub mod store;
 pub mod table;
