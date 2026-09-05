d7afadb296caccd27ee02c636eeac1305542d85b
mroczect <mroczect@proton.me>
2026-09-06 02:20:26 +0700

    feat(functions): export math scalar functions
    Reorganize exports and make aggregate and scalar modules pub(crate).
    Add public re-exports for all new math scalar functions from the math module.
    This allows downstream users to import them directly from monumentum_dsl.

--- monumentum_dsl/src/functions/mod.rs
+++ monumentum_dsl/src/functions/mod.rs
@@ -3,13 +3,36 @@ use alloc::boxed::Box;
 use monumentum_handler::core::value::Value;
 use monumentum_handler::error::DbError;
 
-mod aggregate;
+#[allow(clippy::redundant_pub_crate)]
+pub(crate) mod aggregate;
 mod registry;
-mod scalar;
+#[allow(clippy::redundant_pub_crate)]
+pub(crate) mod scalar;
 
+pub use aggregate::{
+    avg::AvgFunction,
+    count::CountFunction,
+    extras::{
+        GroupConcatFunction, MedianFunction, PercentileContFunction, PercentileDiscFunction,
+        StringAggFunction, TotalFunction,
+    },
+    max::MaxFunction,
+    min::MinFunction,
+    sum::SumFunction,
+};
 pub use registry::FunctionRegistry;
 pub use scalar::{
-    concat::ConcatFunction, length::LengthFunction, lower::LowerFunction, upper::UpperFunction,
+    concat::ConcatFunction,
+    length::LengthFunction,
+    lower::LowerFunction,
+    math::{
+        AcosFunction, AcoshFunction, AsinFunction, AsinhFunction, Atan2Function, AtanFunction,
+        AtanhFunction, CeilFunction, CeilingFunction, CosFunction, CoshFunction, DegreesFunction,
+        ExpFunction, FloorFunction, LnFunction, Log2Function, Log10Function, LogFunction,
+        ModFunction, PiFunction, PowFunction, PowerFunction, RadiansFunction, SinFunction,
+        SinhFunction, SqrtFunction, TanFunction, TanhFunction, TruncFunction,
+    },
+    upper::UpperFunction,
 };
 
 pub trait ScalarFunction: Send + Sync {
@@ -26,15 +49,3 @@ pub trait Accumulator: Send + Sync {
     fn update(&mut self, value: &Value) -> Result<(), DbError>;
     fn finish(self: Box<Self>) -> Result<Value, DbError>;
 }
-
-pub use aggregate::{
-    avg::AvgFunction,
-    count::CountFunction,
-    extras::{
-        GroupConcatFunction, MedianFunction, PercentileContFunction, PercentileDiscFunction,
-        StringAggFunction, TotalFunction,
-    },
-    max::MaxFunction,
-    min::MinFunction,
-    sum::SumFunction,
-};
