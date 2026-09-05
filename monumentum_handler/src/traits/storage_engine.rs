d83ffe33a5c4a8eea9f80532aad76c205f14282c
mroczect <mroczect@proton.me>
2026-09-05 17:59:06 +0700

    feat(handler): expand storage engine trait

--- monumentum_handler/src/traits/storage_engine.rs
+++ monumentum_handler/src/traits/storage_engine.rs
@@ -1,8 +1,21 @@
+use crate::core::row::Row;
 use crate::core::schema::table_schema::TableSchema;
+use crate::core::value::Value;
 use crate::error::DbError;
 
 pub trait StorageEngine {
-    fn load_catalog(&mut self) -> Result<(), DbError>;
-    fn save_catalog(&mut self) -> Result<(), DbError>;
     fn create_table(&mut self, schema: TableSchema) -> Result<(), DbError>;
+    fn drop_table(&mut self, name: &str) -> Result<(), DbError>;
+    fn rename_table(&mut self, old_name: &str, new_name: &str) -> Result<(), DbError>;
+    fn insert_row(&mut self, table: &str, row: &Row) -> Result<(), DbError>;
+    fn get_row(&self, table: &str, row_idx: usize) -> Result<Option<Row>, DbError>;
+    fn set_cell(
+        &mut self,
+        table: &str,
+        row_idx: usize,
+        col_idx: usize,
+        value: Value,
+    ) -> Result<(), DbError>;
+    fn replace_rows(&mut self, table: &str, rows: Vec<Row>) -> Result<(), DbError>;
+    fn checkpoint(&mut self) -> Result<(), DbError>;
 }
