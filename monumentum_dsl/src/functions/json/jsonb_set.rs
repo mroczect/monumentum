b15497cc9befff784661ea104e2e2b07c5e8fd56
mroczect <mroczect@proton.me>
2026-09-06 19:51:45 +0700

    feat(json): implement jsonb_set function
    Add JsonbSetFunction that delegates to JsonSetFunction.

--- monumentum_dsl/src/functions/json/jsonb_set.rs
+++ monumentum_dsl/src/functions/json/jsonb_set.rs
@@ -1,1 +1,20 @@
 
+#![allow(clippy::all)]
+use monumentum_handler::core::value::Value;
+use monumentum_handler::error::DbError;
+
+use crate::functions::ScalarFunction;
+use crate::functions::json::json_set::JsonSetFunction;
+
+#[derive(Debug, Clone, Copy)]
+pub struct JsonbSetFunction;
+
+impl ScalarFunction for JsonbSetFunction {
+    fn name(&self) -> &'static str {
+        "jsonb_set"
+    }
+
+    fn call(&self, args: &[Value]) -> Result<Value, DbError> {
+        JsonSetFunction.call(args)
+    }
+}
