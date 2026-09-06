5c7b88dbaa982cc04069096ae35d22995f45c698
mroczect <mroczect@proton.me>
2026-09-06 19:51:44 +0700

    feat(json): implement jsonb_insert function
    Add JsonbInsertFunction that delegates to JsonInsertFunction.

--- monumentum_dsl/src/functions/json/jsonb_insert.rs
+++ monumentum_dsl/src/functions/json/jsonb_insert.rs
@@ -1,1 +1,20 @@
 
+#![allow(clippy::all)]
+use monumentum_handler::core::value::Value;
+use monumentum_handler::error::DbError;
+
+use crate::functions::ScalarFunction;
+use crate::functions::json::json_insert::JsonInsertFunction;
+
+#[derive(Debug, Clone, Copy)]
+pub struct JsonbInsertFunction;
+
+impl ScalarFunction for JsonbInsertFunction {
+    fn name(&self) -> &'static str {
+        "jsonb_insert"
+    }
+
+    fn call(&self, args: &[Value]) -> Result<Value, DbError> {
+        JsonInsertFunction.call(args)
+    }
+}
